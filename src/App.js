af4130da260c8dc46bb0cd0510e7f08c9f91c7d2
twalk2 <2twalk2@gmail.com>
2019-11-11 10:29:44 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import Button from "./Button";
+import * as math from "mathjs";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ops: [
+        ["7", "8", "9", "/"],
+        ["4", "5", "6", "*"],
+        ["1", "2", "3", "+"],
+        ["^", "0", ".", "-"]
+      ],
+      input: ""
+    };
+  }
+
+  addToInput = val => {
+    this.setState({ input: this.state.input + val });
+  };
+
+  handleEqual = () => {
+    this.setState({ input: math.evaluate(this.state.input) });
+  };
+
+  renderButtons = () => {
+    return this.state.ops.map(row => {
+      return (
+        <div className="row">
+          {row.map(digit => (
+            <Button handleClick={this.addToInput}>{digit}</Button>
+          ))}
+        </div>
+      );
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="input">{this.state.input}</div>
+        {this.renderButtons()}
+        <div className="row">
+          <Button
+            label="clear-btn"
+            handleClick={() => this.setState({ input: "" })}
+          >
+            Clear
+          </Button>
+          <Button label="equal-btn" handleClick={this.handleEqual}>
+            =
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Button = props => {
+  return (
+    <div
+      className={`button-wrapper
+    ${isNaN(props.children) && "operator"}
+    ${props.label}
+  `}
+      onClick={() => props.handleClick(props.children)}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+export default Button;
